40a9a4c1ea20c141717e14454f7d3e3cd28cf850
dellon <djones1@kippnyccp.org>
2015-03-11 09:43:43 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -3,10 +3,13 @@ $(document).ready(function(){
   for(i in my_array){
     $('#card_holder').append('<div class="card"><p>'+my_array[i]+'</p></div>');
   }
-
+  var click_counter=0;
+  var card1=0;
+  var card2=0;
   $(".card").click(function(){
     if(click_counter==0){
       click_counter == 1;
+      card1=$(this).find("p").text();
     	$(this).find("p").css("opacity", "1");}
     else{
       
