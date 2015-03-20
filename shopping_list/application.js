46d193ff2395fff8713bec2bd1603bce288909bc
dellon <djones1@kippnyccp.org>
2015-03-20 09:05:53 -0400

    Update application.js

--- shopping_list/application.js
+++ shopping_list/application.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
-  $('button').on('click',function(){
+  $('#add').on('click',function(){
     var val1 = $('input').val();
     $('div').append("<p>"+val1+"</p>");
     console.log(val1);
+    $('#clear').on('click',function(){
+    $('div').empty();  
+    });
   });
 });
