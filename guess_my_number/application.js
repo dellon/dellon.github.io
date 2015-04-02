c54f496e530874ef1d3831cb957fcd3a9cf7ca2c
dellon <djones1@kippnyccp.org>
2015-04-02 08:29:01 -0400

    Update application.js

--- guess_my_number/application.js
+++ guess_my_number/application.js
@@ -1,24 +1,17 @@
+
 $(document).ready(function(){
-  //find a way for the program to choose a random number between 1 and 100, save this as a variable
+  var randomNumber = Math.floor((Math.random() * 100) + 1);//find a way for the program to choose a random number between 1 and 100, save this as a variable
   
   
-  //when the player clicks on the 'guess' button
+  //when the player clicks the 'guess' button
   $('button').on('click', function(){
-    //save their guess as a variable
+   var userGuess =  parseInt ($('input').val()); //save their guess in the console
+    console.log(userGuess);
     
-    
-    //and compare this guess to the random number that the computer picked
-    //if the user guessed the correct number...
-    
-      //what happens if the guess is correct?
-      
-    //if the user guessed too high...
-    
-      //update the 'feedback' paragraph to tell them to guess lower
-      
-    //otherwise, the user guessed too low...
-    
-      //update the 'feedback' paragraph to tell them to guess higher
-    
-  });
-});
+    if (userGuess==randomNumber){
+      alert("You Guess Right")
+    } else if (userGuess > randomNumber ){
+      alert("too high")
+    } else if(userGuess < randomNumber){
+      alert("too low")
+    }
